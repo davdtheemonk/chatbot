e715641194ddf0c8c4dcd59ec9d28d57f86d44ff
davdtheemonk <parishniclaus@gmail.com>
2025-03-04 08:51:10 +0300

    feat: signup

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { Button } from "../../Components/Button";
 import InputBox from "../../Components/InputBox";
 
 export const SignUp = () => {
-  return (
+  const [firstname,setFirstname] = useState("")
+  const [lastname,setLastname] = useState("")
+
+  const [email,setEmail]= useState("") 
+   return (
     <div className="h-screen md:px-80 px-5 flex justify-start items-start  py-10 flex flex-col gap-5">
       <div className="bg-white p-5  flex flex-col gap-4 text-sm w-[350px] m-auto justify-center items-center ">
         <p className="font-bold text-xl">Register</p>
@@ -10,19 +15,19 @@ export const SignUp = () => {
           <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content ">
             Firstname
           </p>
-          <InputBox title="John" />
+          <InputBox title="John" value={firstname} setValue={setFirstname}/>
         </div>
         <div className="flex flex-col justify-start items-start gap-2 w-full">
           <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content ">
             Lastname
-          </p>
-          <InputBox title="Doe" />
+          </p
+          <InputBox title="Doe" value={lastname} setValue={setLastname}/>
         </div>
         <div className="flex flex-col justify-start items-start gap-2 w-full">
           <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content ">
             Email
           </p>
-          <InputBox title="doe@demo.com" />
+          <InputBox title="doe@demo.com" value={email} setValue={setEmail} />
         </div>
         <div className="flex flex-col justify-start items-start gap-2 w-full">
           <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content ">
